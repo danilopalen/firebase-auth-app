45fdb4a22bdbdf12fc9806c8324e38350c60612f
danilo <jayarpaln@yahoo.com>
2021-01-23 15:12:58 +1300

    change .env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,14 @@
 import firebase from "firebase";
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-// };
-// // Initialize Firebase
-// const fb = firebase.initializeApp(firebaseConfig);
-
-// export default fb;
-
-var firebaseConfig = {
-  apiKey: "AIzaSyBYUuUPFUXA14pX-Y3IU3hWJ_4TCoWCwI0",
-  authDomain: "authnew-c7b31.firebaseapp.com",
-  projectId: "authnew-c7b31",
-  storageBucket: "authnew-c7b31.appspot.com",
-  messagingSenderId: "659866505851",
-  appId: "1:659866505851:web:6b9f256c13b0a93114c8de",
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
 // Initialize Firebase
 const fb = firebase.initializeApp(firebaseConfig);
 export default fb;
